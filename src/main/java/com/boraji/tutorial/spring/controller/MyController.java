7af9e9ff9c280d5862f40066b4e2100b6c4b5d01
Xuan Bach <bachcx.hust@gmail.com>
2018-10-11 16:56:11 +0700

    Decode Rss Url

--- src/main/java/com/boraji/tutorial/spring/controller/MyController.java
+++ src/main/java/com/boraji/tutorial/spring/controller/MyController.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
@@ -107,6 +108,8 @@ public class MyController {
 				rss.setLinkRss(URLEncoder.encode(rss.getLinkRss(), "UTF-8"));
 				rss.setNameRss(rootNode.getChildren().get(0).getChildText("generator"));
 				rssService.save(rss);
+				// decode to view
+				rss.setLinkRss(URLDecoder.decode(rss.getLinkRss(), "UTF-8"));
 			}else {
 			
 			}
